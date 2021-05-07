792ca4ede4af5c3f14199daf1efa545a671e4e94
JayMunk <70596902+JayMunk@users.noreply.github.com>
2021-05-07 15:17:56 -0400

    Add files via upload

---
+++ FrontEnd/src/app/models/citoyen.ts
@@ -0,0 +1,13 @@
+export class Citoyen {
+    idUser:number;
+    prenom:string;
+    nom:string;
+    email:string;
+    password:string;
+    age:number;
+    idTuteur:number;
+    numAssuranceSocial:string;
+    numTelephone:string;
+    sexe:string;
+    ville:string;
+}
